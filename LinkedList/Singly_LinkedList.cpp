eaf099f15e253ef1f69425489492e7029f8444d0
Omar Adel <OmarAdel.connect@gmail.com>
2025-11-09 04:17:35 +0200

    Add LinkedList folder

---
+++ LinkedList/Singly_LinkedList.cpp
@@ -0,0 +1,20 @@
+// بسم الله الرحمن الرحيم
+// "وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ"
+// Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(x) (int)(x).size()
+#define updmax(a, b) a = max(a, b)
+#define updmin(a, b) a = min(a, b)
+#define nl '\n'
+#define pi acos(-1.0)
+
+int main()
+{
+
+    return 0;
+}
