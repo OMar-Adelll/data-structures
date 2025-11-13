50585d313cb2431f39eff443ddf288bee90f29a3
Omar Adel <OmarAdel.connect@gmail.com>
2025-11-13 21:44:05 +0200

    Add functions

--- LinkedList/Singly_LinkedList.cpp
+++ LinkedList/Singly_LinkedList.cpp
@@ -13,55 +13,125 @@ using namespace std;
 #define nl '\n'
 #define pi acos(-1.0)
 
+template <typename T>
 struct Node
 {
-    int val;
-    Node *next;
+    T val;
+    Node<T> *next;
 
-    Node(int item) : val(item) {};
+    Node(T item) : val(item) {};
 };
 
+template <typename T>
 class LinkedList
 {
 private:
-    Node *head;
-    Node *tail;
-    int len;
+    Node<T> *head;
+    int length;
 
 public:
-    LinkedList() : head(NULL), tail(NULL), len(0) {}
-
-    bool isempty()
+    LinkedList() : head(nullptr), length(0) {}
+    LinkedList(T item)
     {
-        return head == NULL;
+        Node<T> *node = new Node<T>(item);
+        node->next = nullptr;
+        head = node;
+        length = 1;
     }
-    void insertEnd(int item)
+
+    // -- Main functions -- //
+    bool isempty() { return head == nullptr; }
+
+    void insertFront(T item)
     {
-        Node *node = new Node(item);
+        Node<T> *node = new Node(item);
+        node->next = head;
+        head = node;
+        length++;
+    }
+
+    void insertBack(T item)
+    {
+        Node<T> *node = new Node<T>(item);
 
         if (isempty())
         {
             head = node;
+            length++;
             return;
         }
 
-        Node *trav = head;
-        while (trav->next != NULL)
+        Node<T> *trav = head;
+        while (trav->next != nullptr)
             trav = trav->next;
 
         trav->next = node;
+        length++;
     }
 
     void display()
     {
-        Node *trav = head;
-        while (trav != NULL)
-            cout << trav->val << " ", trav = trav->next;
+        Node<T> *trav = head;
+        while (trav != nullptr)
+        {
+            cout << trav->val << " ";
+            trav = trav->next;
+        }
         cout << nl;
+    }
+
+    // -- Additional funcitons -- //
+
+    Node<T> *get_nth(int num) // nodes is starting from 0....n - 1
+    {
+        if (isempty() || num < 0 || num >= length)
+        {
+            cout << (isempty() ? "Your list is empty !" : "Invalid Index !") << nl;
+            ;
+            exit(1);
+        }
+
+        int cur = 0;
+        Node<T> *trav = head;
+        while (trav != nullptr)
+        {
+            if (cur++ == num)
+                return trav;
+
+            trav = trav->next;
+        }
+
+        return nullptr;
+    }
+
+    int searchItem(T item)
+    {
+        if (isempty())
+        {
+            cout << "Your list is empty !" << nl;
+            exit(1);
+        }
+        int Index = 0;
+        Node<T> *trav = head;
+        while (trav != nullptr)
+        {
+            if (trav->val == item)
+                return Index;
+
+            Index++;
+            trav = trav->next;
+        }
+
+        return -1;
     }
 };
 
 int main()
 {
+    LinkedList<int> list(8);
+
+    int idx = list.searchItem(7);
+
+    cout << idx << nl;
     return 0;
 }
