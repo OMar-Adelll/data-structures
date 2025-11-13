9bc46673e3f6e29b3fc57552ed81856bf8936d1c
Omar Adel <OmarAdel.connect@gmail.com>
2025-11-13 16:50:36 +0200

    Start implementing LinkedList

--- LinkedList/Singly_LinkedList.cpp
+++ LinkedList/Singly_LinkedList.cpp
@@ -13,8 +13,55 @@ using namespace std;
 #define nl '\n'
 #define pi acos(-1.0)
 
+struct Node
+{
+    int val;
+    Node *next;
+
+    Node(int item) : val(item) {};
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int len;
+
+public:
+    LinkedList() : head(NULL), tail(NULL), len(0) {}
+
+    bool isempty()
+    {
+        return head == NULL;
+    }
+    void insertEnd(int item)
+    {
+        Node *node = new Node(item);
+
+        if (isempty())
+        {
+            head = node;
+            return;
+        }
+
+        Node *trav = head;
+        while (trav->next != NULL)
+            trav = trav->next;
+
+        trav->next = node;
+    }
+
+    void display()
+    {
+        Node *trav = head;
+        while (trav != NULL)
+            cout << trav->val << " ", trav = trav->next;
+        cout << nl;
+    }
+};
+
 int main()
 {
-
     return 0;
 }
