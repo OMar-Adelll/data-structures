cf008ee4f7862e635ea481aa6030b4455b4c8e9c
Omar Adel <OmarAdel.connect@gmail.com>
2025-11-17 21:46:33 +0200

    [876] [Time Beats: 100.00%] [Memory Beats: 89.05%] - LeetPush

---
+++ ~DS_LeetCode/LinkedList/Middle-of-the-Linked-List.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (not head->next)
+            return head;
+
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next)
+            fast = fast->next->next, slow = slow->next;
+
+        if(fast->next == nullptr)
+            return slow;
+        return slow->next;
+    }
+};
