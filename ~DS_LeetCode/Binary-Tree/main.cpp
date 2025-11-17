26e1b73eb35c8e8fd1eee6cd452c8eb00ab0b461
Omar Adel <OmarAdel.connect@gmail.com>
2025-11-17 20:30:13 +0200

    Fix file Indexing

---
+++ ~DS_LeetCode/Binary-Tree/main.cpp
@@ -0,0 +1,15 @@
+
+
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
