b5f7c18615f93a2f11d819d5afd8af000b5f0b92
Omar Adel <OmarAdel.connect@gmail.com>
2025-12-16 23:12:27 +0200

    BST

---
+++ Trees/BinarySearchTree/BST.cpp
@@ -0,0 +1,39 @@
+// بسم الله الرحمن الرحيم
+// "وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ"
+// Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define fixed(n) fixed << setprecision(n)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(x) (int)(x).size()
+#define Mod 1000000007
+#define EPS 1e-9
+#define updmax(a, b) a = max(a, b)
+#define updmin(a, b) a = min(a, b)
+#define nl '\n'
+#define pi acos(-1.0)
+void debug() { cout << "[DEBUG]" << nl; }
+
+void mora_solve()
+{
+    
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int q = 1;
+    // cin >> q;
+    for (int tc = 1; tc <= q; tc++)
+    {
+        mora_solve();
+        // if (tc != q) cout << nl;
+    }
+    return 0;
+}
