b2bdc85598ca08bfebbc8edaa4b040a942bc38d1
Omar Adel <OmarAdel.connect@gmail.com>
2025-12-16 08:33:23 +0200

    Trees

---
+++ Trees/BinaryTree.cpp
@@ -0,0 +1,46 @@
+// بسم الله الرحمن الرحيم
+// "وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ"
+// Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define fixed(n) fixed << setprecision(n)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(x) (int)(x).size()
+#define Mod 1000000007
+#define EPS 1e-9
+#define updmax(a, b) a = max(a, b)
+#define updmin(a, b) a = min(a, b)
+#define nl '\n'
+#define pi acos(-1.0)
+void debug() { cout << "[DEBUG]" << nl; }
+
+template <typename T>
+class Node
+{
+public:
+    T val;
+    Node *left;
+    Node *right;
+
+    Node(T v) : val(v), left(nullptr), right(nullptr) {}
+};
+
+template <typename T>
+class BinaryTree
+{
+private:
+    Node *root;
+public:
+    BinaryTree() {root = nullptr};
+
+};
+
+signed main()
+{
+
+    return 0;
+}
